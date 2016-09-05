cc6f2e077b203502deb046a8508df7a7ab6b5ff8
Alexandre Barbieri <alebarbieri1>
2016-09-05 19:20:20 -0300

    Tarefa ES - 05/09/16
    - Criação da classe principal para validar o funcionamento;
    - Alteração dos modificadores de acesso da classe PapelMoeda;
    - Correção dos métodos inserir, getTroco e imprimir da classe
    TicketMachine;
    - Correção do construtor da classe Troco, que cria o array papeisMoeda;
    - Correção dos métodos hasNext, next e remove da classe TrocoIterator;
    - Override do método getMessage das classes de Exception;

--- Source Code Inspection/src/br/calebe/ticketmachine/exception/PapelMoedaInvalidaException.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/exception/PapelMoedaInvalidaException.java
@@ -5,5 +5,10 @@ package br.calebe.ticketmachine.exception;
  * @author Calebe de Paula Bianchini
  */
 public class PapelMoedaInvalidaException extends Exception {
+
+    @Override
+    public String getMessage() {
+        return "Nota inválida!";
+    }
     
 }
--- Source Code Inspection/src/br/calebe/ticketmachine/exception/SaldoInsuficienteException.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/exception/SaldoInsuficienteException.java
@@ -5,4 +5,9 @@ package br.calebe.ticketmachine.exception;
  * @author Calebe de Paula Bianchini
  */
 public class SaldoInsuficienteException extends Exception {
+
+    @Override
+    public String getMessage() {
+        return "Saldo Insuficiente!";
+    }
 }
